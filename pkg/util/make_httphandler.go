cf5a64eb56e3bae39f7350ed044b17507b1f21c5
Leon <coding@leonlonsdale.dev>
2025-08-04 12:45:18 +0100

    refactor: move Make helper to utils out of router

---
+++ pkg/util/make_httphandler.go
@@ -0,0 +1,21 @@
+// Package utils exposes helper utility functions.
+package utils
+
+import (
+	"log/slog"
+	"net/http"
+
+	"github.com/leonlonsdale/projectify/internal/errs"
+)
+
+type HTTPHandler func(w http.ResponseWriter, r *http.Request) error
+
+func Make(f HTTPHandler) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		if err := f(w, r); err != nil {
+			slog.Error("HTTP API Error", "err", err.Error(), "path", r.URL.Path)
+			jsonResponse := errs.ToJSON(err)
+			_ = WriteJSON(w, jsonResponse.Status, jsonResponse)
+		}
+	}
+}
