b1234ae6fdbd63c2605c9d16582f677b9852e594
Leon <coding@leonlonsdale.dev>
2025-08-05 09:34:09 +0100

    refactor: move customer models to models package

--- internal/storage/customerstore/customers.go
+++ internal/storage/customerstore/customers.go
@@ -4,44 +4,11 @@ package customerstore
 import (
 	"context"
 	"database/sql"
-	"errors"
-	"time"
 
 	"github.com/google/uuid"
-	"github.com/leonlonsdale/projectify/pkg/pgutils"
-)
-
-type CustomerRecord struct {
-	ID        uuid.UUID `json:"id"`
-	CreatedAt time.Time `json:"created_at"`
-	UpdatedAt time.Time `json:"updated_at"`
-	Name      string    `json:"name"`
-	Email     string    `json:"email"`
-	Password  string    `json:"password"`
-}
-
-type CustomerRegistration struct {
-	Name     string `json:"name"`
-	Email    string `json:"email"`
-	Password string `json:"password"`
-}
-
-type CustomerUpdate struct {
-	Name     string `json:"name,omitempty"`
-	Email    string `json:"email,omitempty"`
-	Password string `json:"password,omitempty"`
-}
-
-type CustomerSafe struct {
-	ID        uuid.UUID `json:"id"`
-	CreatedAt time.Time `json:"created_at"`
-	UpdatedAt time.Time `json:"updated_at"`
-	Name      string    `json:"name"`
-	Email     string    `json:"email"`
-}
-
-var (
-	ErrEmailAlreadyExists = errors.New("email already exists")
+	"github.com/leonlonsdale/projectify/internal/errs"
+	"github.com/leonlonsdale/projectify/internal/models"
+	"github.com/leonlonsdale/projectify/pkg/utils/pgutils"
 )
 
 type CustomerStorage struct {
@@ -54,7 +21,7 @@ func NewCustomerStorage(db *sql.DB) *CustomerStorage {
 	}
 }
 
-func (s *CustomerStorage) Create(ctx context.Context, data CustomerRegistration) (*CustomerSafe, error) {
+func (s *CustomerStorage) Create(ctx context.Context, data models.CustomerRegistration) (*models.CustomerSafe, error) {
 	query := `
 		INSERT INTO customers (id, created_at, updated_at, name, email, password)
 		VALUES (gen_random_uuid(), NOW(), NOW(), $1, $2, $3)
@@ -63,7 +30,7 @@ func (s *CustomerStorage) Create(ctx context.Context, data CustomerRegistration)
 
 	row := s.db.QueryRowContext(ctx, query, data.Name, data.Email, data.Password)
 
-	var u CustomerSafe
+	var u models.CustomerSafe
 	err := row.Scan(
 		&u.ID,
 		&u.CreatedAt,
@@ -75,7 +42,7 @@ func (s *CustomerStorage) Create(ctx context.Context, data CustomerRegistration)
 		mappedErr := pgutils.MapPgError(err)
 
 		if mappedErr.Error() == "unique_constraint_violation" {
-			return nil, ErrEmailAlreadyExists
+			return nil, errs.ErrEmailAlreadyExists
 		}
 
 		return nil, mappedErr
@@ -84,16 +51,16 @@ func (s *CustomerStorage) Create(ctx context.Context, data CustomerRegistration)
 	return &u, nil
 }
 
-func (s *CustomerStorage) GetByID(ctx context.Context, id uuid.UUID) (CustomerSafe, error) {
-	return CustomerSafe{}, nil
+func (s *CustomerStorage) GetByID(ctx context.Context, id uuid.UUID) (models.CustomerSafe, error) {
+	return models.CustomerSafe{}, nil
 }
 
-func (s *CustomerStorage) GetByEmail(ctx context.Context, email string) (CustomerSafe, error) {
-	return CustomerSafe{}, nil
+func (s *CustomerStorage) GetByEmail(ctx context.Context, email string) (models.CustomerSafe, error) {
+	return models.CustomerSafe{}, nil
 }
 
-func (s *CustomerStorage) Update(ctx context.Context, data CustomerUpdate, id uuid.UUID) (CustomerSafe, error) {
-	return CustomerSafe{}, nil
+func (s *CustomerStorage) Update(ctx context.Context, data models.CustomerUpdate, id uuid.UUID) (models.CustomerSafe, error) {
+	return models.CustomerSafe{}, nil
 }
 
 func (s *CustomerStorage) Delete(ctx context.Context, id uuid.UUID) error {
