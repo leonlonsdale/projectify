5e8f01d39f42af241b219be1f4be696f54406b62
Leon <coding@leonlonsdale.dev>
2025-08-05 09:35:27 +0100

    feat: add HandleCreateCustomer route handler

--- internal/router/customer/customer.go
+++ internal/router/customer/customer.go
@@ -2,29 +2,76 @@
 package customerhandlers
 
 import (
+	"errors"
 	"net/http"
 
+	"github.com/leonlonsdale/projectify/internal/auth"
+	"github.com/leonlonsdale/projectify/internal/errs"
+	"github.com/leonlonsdale/projectify/internal/models"
 	"github.com/leonlonsdale/projectify/internal/storage"
-	utils "github.com/leonlonsdale/projectify/pkg/util"
+	"github.com/leonlonsdale/projectify/pkg/utils/httputils"
+	"github.com/leonlonsdale/projectify/pkg/utils/jsonutils"
 )
 
 type CustomerHandler struct {
 	store *storage.Storage
+	auth  *auth.Auth
 }
 
-func NewCustomerHandler(store *storage.Storage) *CustomerHandler {
-	return &CustomerHandler{store}
+func NewCustomerHandler(store *storage.Storage, auth *auth.Auth) *CustomerHandler {
+	return &CustomerHandler{
+		store: store,
+		auth:  auth,
+	}
 }
 
 func (h *CustomerHandler) Register(m *http.ServeMux) {
-	m.Handle("POST /customer", utils.Make(h.HandleCreateCustomer))
-	m.Handle("GET /customer/{id}", utils.Make(h.HandleGetCustomerByID))
-	m.Handle("GET /customer", utils.Make(h.HandleGetCustomerByEmail))
-	m.Handle("PUT /customer/{id}", utils.Make(h.HandleUpdateCustomer))
-	m.Handle("DELETE /customer/{id}", utils.Make(h.HandleDeleteCustomer))
+	m.Handle("POST /customer", httputils.Make(h.HandleCreateCustomer))
+	m.Handle("GET /customer/{id}", httputils.Make(h.HandleGetCustomerByID))
+	m.Handle("GET /customer", httputils.Make(h.HandleGetCustomerByEmail))
+	m.Handle("PUT /customer/{id}", httputils.Make(h.HandleUpdateCustomer))
+	m.Handle("DELETE /customer/{id}", httputils.Make(h.HandleDeleteCustomer))
 }
 
 func (h *CustomerHandler) HandleCreateCustomer(w http.ResponseWriter, r *http.Request) error {
+
+	ctx := r.Context()
+	var params models.CustomerRegistration
+	if err := jsonutils.DecodeJSON(r.Body, &params); err != nil {
+		return errs.NewBadRequest("malformed request body", err)
+	}
+
+	if validationErrs := params.Validate(); validationErrs != nil {
+		return errs.NewValidationErrors(validationErrs)
+	}
+
+	hashedPass, err := h.auth.Password.Hash(params.Password)
+	if err != nil {
+		return errs.NewInternalServerError("error hashing password", err)
+	}
+
+	params.Password = hashedPass
+
+	user, err := h.store.Customers.Create(ctx, params)
+	if err != nil {
+		if errors.Is(err, errs.ErrEmailAlreadyExists) {
+			return errs.NewBadRequest("Could not create customer", err)
+		}
+		return errs.NewInternalServerError("failed to create customer", err)
+	}
+
+	safeUser := models.CustomerSafe{
+		ID:        user.ID,
+		CreatedAt: user.CreatedAt,
+		UpdatedAt: user.UpdatedAt,
+		Name:      user.Name,
+		Email:     user.Email,
+	}
+
+	if err := jsonutils.WriteJSON(w, http.StatusCreated, safeUser); err != nil {
+		return errs.NewInternalServerError("error writing json", err)
+	}
+
 	return nil
 }
 
