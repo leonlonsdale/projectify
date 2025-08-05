b81db6819547087e86c7e25a5ff1493e3e0f5f3a
Leon <coding@leonlonsdale.dev>
2025-08-05 15:11:31 +0100

    feat: add customer store update query

--- internal/storage/customerstore/customers.go
+++ internal/storage/customerstore/customers.go
@@ -4,6 +4,7 @@ package customerstore
 import (
 	"context"
 	"database/sql"
+	"errors"
 
 	"github.com/google/uuid"
 	"github.com/leonlonsdale/projectify/internal/errs"
@@ -52,6 +53,7 @@ func (s *CustomerStorage) Create(ctx context.Context, data models.CustomerRegist
 }
 
 func (s *CustomerStorage) GetByID(ctx context.Context, id uuid.UUID) (models.CustomerSafe, error) {
+
 	return models.CustomerSafe{}, nil
 }
 
@@ -59,8 +61,37 @@ func (s *CustomerStorage) GetByEmail(ctx context.Context, email string) (models.
 	return models.CustomerSafe{}, nil
 }
 
-func (s *CustomerStorage) Update(ctx context.Context, data models.CustomerUpdate, id uuid.UUID) (models.CustomerSafe, error) {
-	return models.CustomerSafe{}, nil
+func (s *CustomerStorage) Update(ctx context.Context, data models.CustomerUpdate, id uuid.UUID) (*models.CustomerSafe, error) {
+	query := `
+		UPDATE customers
+		SET
+			name = COALESCE($1, name),
+			email = COALESCE($2, name),
+			updated_at = NOW()
+		WHERE id = $3
+		RETURNING id, created_at, updated_at, name, email;
+	`
+
+	row := s.db.QueryRowContext(ctx, query, data.Name, data.Email, id)
+
+	var u models.CustomerSafe
+	err := row.Scan(
+		&u.ID,
+		&u.CreatedAt,
+		&u.UpdatedAt,
+		&u.Name,
+		&u.Email,
+	)
+	if err != nil {
+		mappedErr := pgutils.MapPgError(err)
+		if errors.Is(mappedErr, sql.ErrNoRows) {
+			return nil, errs.ErrUserNotFound
+		}
+
+		return nil, err
+	}
+
+	return &u, nil
 }
 
 func (s *CustomerStorage) Delete(ctx context.Context, id uuid.UUID) error {
