a2a66bebffc8654c7f738c8f6049ec18861a809a
Leon <coding@leonlonsdale.dev>
2025-08-05 19:45:35 +0100

    feat: add jwt error definitions

--- internal/errs/definitions.go
+++ internal/errs/definitions.go
@@ -3,7 +3,12 @@ package errs
 import "errors"
 
 var (
-	ErrInvalidCredentials = errors.New("username or password incorrect")
-	ErrEmailAlreadyExists = errors.New("email already exists")
-	ErrUserNotFound       = errors.New("user not found")
+	ErrInvalidCredentials        = errors.New("username or password incorrect")
+	ErrEmailAlreadyExists        = errors.New("email already exists")
+	ErrUserNotFound              = errors.New("user not found")
+	ErrorJWTInvalidSigningMethod = errors.New("invalid signing method")
+	ErrorJWTInvalidToken         = errors.New("invalid jwt token")
+	ErrorJWTInvalidClaimsType    = errors.New("invalid claims type")
+	ErrorJWTTokenHasExpired      = errors.New("jwt token has expired")
+	ErrorNoBearerToken           = errors.New("no bearer token in headers")
 )
