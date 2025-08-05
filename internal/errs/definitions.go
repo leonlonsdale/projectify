e496b48f28957c68f12a8770689fcb78ca6b58c8
Leon <coding@leonlonsdale.dev>
2025-08-05 15:11:45 +0100

    feat: add user not found err

--- internal/errs/definitions.go
+++ internal/errs/definitions.go
@@ -5,4 +5,5 @@ import "errors"
 var (
 	ErrInvalidCredentials = errors.New("username or password incorrect")
 	ErrEmailAlreadyExists = errors.New("email already exists")
+	ErrUserNotFound       = errors.New("user not found")
 )
