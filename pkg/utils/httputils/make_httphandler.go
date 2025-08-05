418ff3d24fa2903c8f6041beecec7bf2152453a9
Leon <coding@leonlonsdale.dev>
2025-08-05 09:31:42 +0100

    refactor: reorganise utils to domain type utils

--- pkg/utils/httputils/make_httphandler.go
+++ pkg/utils/httputils/make_httphandler.go
@@ -1,11 +1,12 @@
-// Package utils exposes helper utility functions.
-package utils
+// Package httputils exposes helper utility functions for http.
+package httputils
 
 import (
 	"log/slog"
 	"net/http"
 
 	"github.com/leonlonsdale/projectify/internal/errs"
+	"github.com/leonlonsdale/projectify/pkg/utils/jsonutils"
 )
 
 type HTTPHandler func(w http.ResponseWriter, r *http.Request) error
@@ -14,8 +15,8 @@ func Make(f HTTPHandler) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		if err := f(w, r); err != nil {
 			slog.Error("API Error", "err", err.Error(), "path", r.URL.Path)
-			jsonResponse := errs.ToJSON(err)
-			_ = WriteJSON(w, jsonResponse.Status, jsonResponse)
+			jsonResponse := errs.ErrToJSON(err)
+			_ = jsonutils.WriteJSON(w, jsonResponse.Status, jsonResponse)
 		}
 	}
 }
