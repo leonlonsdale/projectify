57c75472c10b08ea6b85934e992183bdc6c5fe9b
Leon <coding@leonlonsdale.dev>
2025-08-05 19:46:11 +0100

    feat: add jwt to auth layer

--- internal/auth/auth.go
+++ internal/auth/auth.go
@@ -1,14 +1,20 @@
 // Package auth exposes an Auth layer which exposes password and jwt functionality
 package auth
 
-import "github.com/leonlonsdale/projectify/internal/auth/password"
+import (
+	"github.com/leonlonsdale/projectify/internal/auth/jwt"
+	"github.com/leonlonsdale/projectify/internal/auth/password"
+	"github.com/leonlonsdale/projectify/internal/config"
+)
 
 type Auth struct {
 	Password *password.A2idpassword
+	JWT      *jwt.JWT
 }
 
-func NewAuth() *Auth {
+func NewAuth(cfg *config.Config) *Auth {
 	return &Auth{
 		Password: password.NewA2idpassword(),
+		JWT:      jwt.NewJWT(cfg),
 	}
 }
