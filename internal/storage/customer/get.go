16e49473c401ba21b91f0569bf5cbc4836407836
Leon <coding@leonlonsdale.dev>
2025-08-06 11:20:16 +0100

    feat: add customer store get func

--- internal/storage/customer/get.go
+++ internal/storage/customer/get.go
@@ -2,16 +2,61 @@ package customerstore
 
 import (
 	"context"
+	"database/sql"
+	"errors"
+	"fmt"
 
 	"github.com/google/uuid"
+	"github.com/leonlonsdale/projectify/internal/errs"
 	"github.com/leonlonsdale/projectify/internal/models"
+	"github.com/leonlonsdale/projectify/pkg/utils/pgutils"
 )
 
-func (s *CustomerStorage) GetByID(ctx context.Context, id uuid.UUID) (models.CustomerSafe, error) {
+func (s *CustomerStorage) get(ctx context.Context, column string, value any) (*models.CustomerSafe, error) {
 
-	return models.CustomerSafe{}, nil
+	validColumns := map[string]struct{}{
+		"id":    {},
+		"email": {},
+	}
+
+	if _, ok := validColumns[column]; !ok {
+		return nil, fmt.Errorf("invalid column provided: %s", column)
+	}
+
+	query := `
+		SELECT id, created_at, updated_at, name, email
+		FROM users
+		WHERE ` + column + ` = $1;
+	`
+
+	row := s.db.QueryRowContext(ctx, query, value)
+
+	var u models.CustomerSafe
+	err := row.Scan(
+		&u.ID,
+		&u.CreatedAt,
+		&u.UpdatedAt,
+		&u.Name,
+		&u.Email,
+	)
+
+	if err != nil {
+		mappedErr := pgutils.MapPgError(err)
+
+		if errors.Is(err, sql.ErrNoRows) {
+			return nil, errs.ErrUserNotFound
+		}
+
+		return nil, mappedErr
+	}
+
+	return &u, nil
 }
 
-func (s *CustomerStorage) GetByEmail(ctx context.Context, email string) (models.CustomerSafe, error) {
-	return models.CustomerSafe{}, nil
+func (s *CustomerStorage) GetByID(ctx context.Context, id uuid.UUID) (*models.CustomerSafe, error) {
+	return s.get(ctx, "id", id)
+}
+
+func (s *CustomerStorage) GetByEmail(ctx context.Context, email string) (*models.CustomerSafe, error) {
+	return s.get(ctx, "email", email)
 }
